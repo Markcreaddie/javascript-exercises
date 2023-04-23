de261cb3cfd1a1ed5b68d01f1d66bac7e1dc0ef1
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-04-23 14:55:56 -0400

    complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    sequence=[0,1]
+    num= +num
+    if (num<0){
+        return "OOPS"
+    }else if(num<2){
+        return sequence[num]
+    }else{
+        for (let i=2; i<=num;i++){
+            sequence.push(sequence[i-1]+sequence[i-2])
+        }
+    }
+    return sequence[num]
 };
 
 // Do not edit below this line
