6254935d69e29058dc9385e002da6a81d2e543fe
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-04-23 06:55:54 -0400

    complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(myArray,...args) {
+    newList = []
+    items: for (item of myArray){
+        for (obj of [...args]){
+            if (item===obj){
+                continue items
+            }
+        } newList.push(item)
+    }return newList
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
