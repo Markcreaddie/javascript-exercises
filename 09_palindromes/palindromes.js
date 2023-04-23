0856886d3d9574c1dce8dd2722d9cfab6c833c60
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-04-23 14:40:00 -0400

    complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    cleanStr=str.replace(/[^a-zA-Z]/g, "").toLowerCase()
+    chars= cleanStr.split("")
+    reverseStr=chars.reverse().join("")
+    return cleanStr===reverseStr
 };
 
 // Do not edit below this line
