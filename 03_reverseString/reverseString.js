216eceea02981e75b148c8ef3d37f8aee1d03fac
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-04-23 06:22:12 -0400

    complete exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    chars = str.split("")
+    chars.reverse()
+    return chars.join("")
 };
 
 // Do not edit below this line
