e1cb6bd1c87631377e44fe229e585f31a80e1874
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-04-23 14:24:40 -0400

    complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+return x-y
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+  let sum=0
+	for (num of myArray){
+    sum+=num
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(myArray) {
+let result=1
+for (num of myArray){
+  result *=num
+}
+return result
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x**y
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num===0){
+    return 1
+  }else{
+    let result=1
+    for (i=1; i<=num; i++){
+      result*=i
+    }
+    return result
+  }
 };
 
 // Do not edit below this line
