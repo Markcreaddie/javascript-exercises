71709a93beebd09fa377e814c97b44a6140d16b9
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-04-23 07:16:51 -0400

    complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (num1<0 ||
+        num2 <0 || 
+        typeof(num1)!=='number'||
+        typeof(num2)!=='number'){
+        return "ERROR"
+    }
 
+    let start;
+    let end;
+    if (num1<num2){
+        start = num1;
+        end = num2;
+    }else{
+        start = num2;
+        end = num1;
+    }
+    sum = 0;
+    for (let i = start; i<=end; i++){
+        sum+=i;
+    }return sum
 };
 
 // Do not edit below this line
