987e4c57bd0b127a7d896f5fabbcb6bc608602a7
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-05-02 15:39:58 -0400

    complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear =new Date().getFullYear()
+    return people.sort((a,b)=>{  
+        a.yearOfDeath?
+            oldest = a.yearOfDeath-a.yearOfBirth:
+            oldest =currentYear-a.yearOfBirth;
+        b.yearOfDeath?
+            next = b.yearOfDeath-b.yearOfBirth:
+            next = currentYear - b.yearOfBirth;
+        return oldest>next?-1:1;
+    })[0]
 };
 
 // Do not edit below this line
