bc7d183f1f18d38068cca41ede98ace320da76e0
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2026-05-24 16:00:15 +0300

    Complete exercise 09_sumAll
    Create sumAll function based on the README.md
    file and run the tests in the .spec file

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
-};
+const sumAll = function (a, b) {
+    if (typeof (a) !== 'number' ||
+        typeof (b) !== 'number' ||
+        !Number.isInteger(a) ||
+        !Number.isInteger(a) ||
+        a < 0 ||
+        b < 0) {
+        return "ERROR"
+    }
+    start = (a < b) ? a : b
+    end = (a < b) ? b : a
+    sum = 0
+    for (i = start; i <= end; i++) {
+        sum += i
+    }
+    return sum
+}
+    ;
 
 // Do not edit below this line
 module.exports = sumAll;
