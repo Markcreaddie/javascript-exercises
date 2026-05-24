572ce8f0688f4d10345a24ada13e86ef8aaac814
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2026-05-24 16:14:21 +0300

    Complete exercise 10_leapYears
    Create leapYears function based on the README.md
    file and run the tests in the .spec file

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    //if year is not divisible by 4, not a leap year
+    if (year % 4 !== 0) {
+        return false
+    }
+    //if year is divisible by 4 but not 100, is a leap year
+    if (year % 100 !== 0) {
+        return true
+    }
+    //if year is divisible by 4, 100 and 400, is a leap year
+    if (year % 400 === 0) {
+        return true
+    } else {
+        //divisible by 4 & 100 but not 400
+        return false
+    }
 
 };
 
