6fdd883f45621dfbc3ab4e3ad0f53e64bf6b3dff
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2026-05-24 15:03:10 +0300

    Complete exercise 08_removeFromArray
    Create removeFromArray function based on the README.md
    file and run the tests in the .spec file

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    result = []
+    for (item of arr) {
+        if (args.includes(item)) {
+            continue
+        }
+        result.push(item)
+    }
+    return result
 };
 
 // Do not edit below this line
