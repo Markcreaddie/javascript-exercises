d2c5e540c61f5989bc93ff0e12716033daa208eb
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2026-06-16 20:00:07 +0300

    Complete exercise 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const validChars = "abcdefghijklmnopqrstuvwxyz1234567890"
+
+    const wordChars = word
+        .toLowerCase()
+        .split('')
+        .filter((char) => validChars.includes(char))
+        .join('')
+
+
+    const reverseWordChars = wordChars.split("").reverse().join("")
+
+    return wordChars === reverseWordChars
 
 };
 
