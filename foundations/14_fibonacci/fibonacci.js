6565d9e1e749b846f7033d149a198ec6b46c8b9f
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2026-06-16 20:25:39 +0300

    Complete exercise 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    num = +num
+    if (num == 0) return 0
+    if (num < 0) return "OOPS"
+    sequence = [1]
+    for (i = 0; i < num; i++) {
+        if (i == 0) {
+            sequence.push(1)
+        } else {
+            sequence.push(sequence[i] + sequence[i - 1])
+        }
+    }
+    return sequence[num - 1]
+
 
 };
 
