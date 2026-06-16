d4e145df9d60869d711fdfce7da78cb61fd08e36
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2026-06-16 21:55:03 +0300

    Complete exercise 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (objArray) {
+    oldestPerson = objArray.reduce((oldest, person) => {
+        if (oldest == "") {
+            oldest = person
+        }
+        let oldestAge = "yearOfDeath" in oldest ? (oldest.yearOfDeath - oldest.yearOfBirth) : new Date().getFullYear() - oldest.yearOfBirth
+        let personAge = "yearOfDeath" in person ? (person.yearOfDeath - person.yearOfBirth) : new Date().getFullYear() - person.yearOfBirth
 
+        return personAge > oldestAge ? person : oldest
+    }, "")
+    return oldestPerson
 };
 
 // Do not edit below this line
